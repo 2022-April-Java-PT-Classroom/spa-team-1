1cbebce4d950fba3c0f03657119c0be7b543e7b8
gianramirez <radicgian@gmail.com>
2022-07-11 22:30:47 -0400

    added texts, stylelinks, logoimg to header, foundational code to pages, routeswwitch to approuter

---
+++ application/src/main/resources/static/js/react-client/src/pages/about-screen/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboutScreen = () => {
+
+    return (
+        <div>
+            <h2>This is the about screen</h2>
+        </div>
+        
+    );
+    
+}
+
+export default AboutScreen;
