c445cf941d3efe3ff4886fab4bf1bcfde849f56d
acolter12 <acolter@kent.edu>
2022-07-14 22:47:39 -0400

    Was able to pull information from met API. So far, only able to pull all needed information from a single object's api, not a collection or search.

--- application/src/main/resources/static/js/react-client/src/pages/artgallery-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/artgallery-screen/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 
+import ArtImages from "./ArtImages";
 import Axios from "axios";
 
 const ArtgalleryScreen = () => {
@@ -9,9 +10,11 @@ const ArtgalleryScreen = () => {
 
     useEffect(() => {
         const fetchArtData = async () => {
-            const result = await Axios('http://collectionapi.metmuseum.org/public/collection/v1/objects');
+            const result = await Axios(`https://collectionapi.metmuseum.org/public/collection/v1/objects/437`);
+            console.log(result.data);
+
             setArt(result.data);
-        }
+        };
 
         if(art) {
             setLoading(false);
@@ -27,11 +30,13 @@ const ArtgalleryScreen = () => {
 
 
     return (
+        loading ? <h3>Creating art...</h3> :
         <div>
             <h2>This is the art Gallery</h2>
-            {loading ? <h3>Creating art...</h3> : art.map(art => (
-            <p>{art.objectName}</p>
-            ))}
+          <h4>Art</h4>
+          <h5>{art.objectID}</h5>
+          <h5>{art.objectName}</h5>
+          <img src={art.primaryImage} />
 
         </div>
         
