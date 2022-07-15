87aa2a0e1762abeffe48ebd6484e4c921e26322c
acolter12 <acolter@kent.edu>
2022-07-14 20:30:51 -0400

    Added js to artgallery index.js file. Having trouble getting art api to show on browser. Added loading element to page.

--- application/src/main/resources/static/js/react-client/src/pages/artgallery-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/artgallery-screen/index.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Axios from "axios";
 
 const ArtgalleryScreen = () => {
 
+    const [art, setArt] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchArtData = async () => {
+            const result = await Axios('http://collectionapi.metmuseum.org/public/collection/v1/objects');
+            setArt(result.data);
+        }
+
+        if(art) {
+            setLoading(false);
+        }
+
+        const timer = setTimeout(() => {
+            !art && fetchArtData();
+        }, 500);
+
+        return () => clearTimeout(timer);
+        }, [art]);
+
+
+
     return (
         <div>
-            <h2>This is the artgallery screen</h2>
+            <h2>This is the art Gallery</h2>
+            {loading ? <h3>Creating art...</h3> : art.map(art => (
+            <p>{art.objectName}</p>
+            ))}
+
         </div>
         
     );
     
-}
 
+};
 export default ArtgalleryScreen;
