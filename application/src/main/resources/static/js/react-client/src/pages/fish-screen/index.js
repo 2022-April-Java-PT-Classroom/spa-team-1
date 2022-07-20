6d1d2b77b7d5a3585ad2c0c6516ecbec994d7016
grullonvictor06 <grullonvictor06@gmail.com>
2022-07-19 23:54:51 -0400

    got info in need to fix pictures and randomizer

--- application/src/main/resources/static/js/react-client/src/pages/fish-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/fish-screen/index.js
@@ -6,11 +6,12 @@ const FishPage = () => {
   
     const [fishes, setFishes] = useState(null);
     const [loading, setLoading] = useState(true);
+    const  radnomNum = 
 
     useEffect(() => {
         const fetchFishesData = async () => {
             const result = await Axios('https://www.fishwatch.gov/api/species');
-            setFishes(result.data[0]);
+            setFishes(result.data[100]);
             console.log(result.data);
         }
         
@@ -30,6 +31,21 @@ return (
     loading ? <h3>loading fish...</h3> :
     <div>
         <h3>{fishes['Species Name']}</h3>
+        <h3>{fishes['Scientific Name']}</h3>
+        <img src={fishes['Image Gallery']}></img>
+        <h4>{fishes['Quote']}</h4>
+        <div>
+            <ul>NOAA Fisheries Region : {fishes['NOAA Fisheries Region']}</ul>
+            <ul>Population : {fishes['Population']}</ul>
+            <ul>Calories : {fishes['Calories']}</ul>
+            <ul>Carbohydrate : {fishes['Carbohydrate']}</ul>
+            <ul>Cholesterol : {fishes['Cholesterol']}</ul>
+            <ul>FatTotal : {fishes['Fat, Total']}</ul>
+            <ul>Fiber, Total Dietary : {fishes['Fiber, Total Dietary']}</ul>
+            <ul>Sodium : {fishes['Sodium']}</ul>
+            
+            
+            </div>
     </div>
     );
 };
