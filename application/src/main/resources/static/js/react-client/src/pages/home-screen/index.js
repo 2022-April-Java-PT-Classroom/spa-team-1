9c237686c9613bd8e2d1b5230143798248b2f870
Gian <90577336+gianramirez@users.noreply.github.com>
2022-07-20 23:29:45 -0400

    Revert "Homeindex gian"

--- application/src/main/resources/static/js/react-client/src/pages/home-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/home-screen/index.js
@@ -1,7 +1,6 @@
 import React, { createContext, useState } from "react";
 
 import ReactSwitch from "react-switch";
-import box from '../../assets/homepagepics/mysterybox.gif';
 import style from './style.module.scss';
 
 export const ThemeContext = createContext(null);
@@ -24,58 +23,9 @@ const Homescreen = () => {
         </div>
 
         <div className={style.Homescreen}>
-            <h2>Welcome to the Mystery Learning Site</h2>
+            <h2>This is the home page</h2>
         </div>
-
-        <section className={style.artsection}>
-          <div className={style.artimg}>
-          <a href="/artgallery"><img className={style.mysterybox} src={box} alt="mystery box" />
-          </a>
-          </div>
-          <div className={style.artText}>
-            <h2>Art Museum</h2>
-            <p>Learn more about history through creativity</p>
-            
-          </div>
-        </section>
-
-        <section className={style.fishsection}>
-          <div className={style.fishimg}>
-          <a href="/fish"><img className={style.mysterybox} src={box} alt="mystery box" />
-          </a>
-          </div>
-          <div className={style.fishText}>
-            <h2>Aquarium</h2>
-            <p>Learn more about fish wildlife</p>
-            
-          </div>
-        </section>
-
-        <section className={style.mathsection}>
-          <div className={style.mathimg}>
-          <a href="/math"><img className={style.mysterybox} src={box} alt="mystery box" />
-          </a>
-          </div>
-          <div className={style.mathText}>
-            <h2>Math Facts</h2>
-            <p>Practice with some fun</p>
-            
-          </div>
-        </section>
-
-        <section className={style.rewardsection}>
-          <div className={style.rewardimg}>
-          <a href="/reward"><img className={style.mysterybox} src={box} alt="mystery box" />
-          </a>
-          </div>
-          <div className={style.artText}>
-            <h2>Rewards</h2>
-            <p>Check out what you've earned!</p>
-            
-          </div>
-        </section>
-
-        
+    
     </div>
     </div>   
     </ThemeContext.Provider>    
