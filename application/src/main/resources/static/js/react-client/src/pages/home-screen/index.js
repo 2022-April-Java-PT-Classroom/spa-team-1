df5c1069699c0d60d45cc659205439af23485703
gianramirez <radicgian@gmail.com>
2022-07-21 01:39:58 -0400

    something

--- application/src/main/resources/static/js/react-client/src/pages/home-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/pages/home-screen/index.js
@@ -2,6 +2,7 @@ import React, { createContext, useState } from "react";
 
 import ReactSwitch from "react-switch";
 import box from '../../assets/homepagepics/mysterybox.gif';
+import mySound from '../../assets/audio/theme.mp3';
 import style from './style.module.scss';
 
 export const ThemeContext = createContext(null);
@@ -12,6 +13,7 @@ const Homescreen = () => {
     const toggleTheme = () => {
       setTheme((curr) => (curr === "light" ? "dark" : "light"));
     };
+      
     
     return (
 
@@ -19,7 +21,6 @@ const Homescreen = () => {
       <div className="Contact" id={theme} >   
       <div className={theme=== 'dark' ? style.dark : style.light}>
       <div className="switch">
-      {/* <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label> */}
         <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
         </div>
 
